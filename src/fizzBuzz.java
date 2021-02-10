e1c206b2236485abafcbdc6d95eef840de33ae3c
James Ashman <jamesashman@jamess-mbp.home>
2021-02-10 12:02:44 +0000

    second commit
    Improves logic for code
    Adds 'woof' logic for multiples of 7
    Added comments

--- src/fizzBuzz.java
+++ src/fizzBuzz.java
@@ -1,79 +1,80 @@
-import java.util.*;
-
 public class fizzBuzz {
     public static void main(String args[])
     {
-        for(int i = 1; i <= 100; i++)
+        for(int i = 1; i <= 100; i++) //Loops from 1 - 100 and applies problem algorithm 
         {
-            IsMultipleOfThree(i);
+            CheckProblem(i); 
         }
     }
 
-    public static void IsMultipleOfThree(int num)
+    public static boolean IsMultipleOfThree(int num) 
+    //Checks if divisible by 3 with no remainder
     {
-        ArrayList<Integer> split = new ArrayList<Integer>();
-        int answer = 0;
-        int num2 = num;
-        boolean isThree = false; 
-        if(num <= 9)
-        {
-            split.add(num);
-        }
-        else
-        {
-            while(num > 0)
-            {
-                int temp = num % 10;
-                split.add(temp);
-                num /= 10;
-            }
-        }
-        for(int i : split)
-        {
-            answer = answer + i;
-        }
-        if(answer % 3 == 0)
+        boolean isThree = false;
+        if(num % 3 == 0)
         {
             isThree = true; 
         }
-        IsMultipleOfFive(num2, isThree);
+        return isThree;
     }
 
-    public static void IsMultipleOfFive(int num, boolean isThree)
+    public static boolean IsMultipleOfFive(int num) 
+    //Checks if divisible by 5 with no remainder
     {
-        int temp = num % 10;
-        switch(temp)
+        boolean isFive = false;
+        if (num % 5 == 0)
         {
-            case 0: 
-                if(isThree == true)
-                {
-                    System.out.println("fizzbuzz");
-                }
-                else
-                {
-                    System.out.println("buzz");
-                }
-                break;
-            case 5:
-                if(isThree == true)
-                {
-                    System.out.println("fizzbuzz");
-                }
-                else
-                {
-                    System.out.println("buzz");
-                }
-                break;
-            default:
-                if(isThree == true)
-                {
-                    System.out.println("fizz");  
-                }
-                else
-                {
-                    System.out.println(num);  
-                }  
+            isFive = true;
+        }
+        return isFive;
+    }
+
+    public static boolean IsMultipleOfSeven(int num) 
+    //Checks if divisible by 7 with no remainder
+    {
+        boolean isSeven = false;
+        if(num % 7 == 0)
+        {
+            isSeven = true;
+        }
+        return isSeven;
+    }
+
+    public static void CheckProblem(int num)
+    /* Applies rules of problem based on if number is divisible by 3, 5, 7 
+    or a combination of these */
+    {
+        boolean three = IsMultipleOfThree(num); 
+        boolean five = IsMultipleOfFive(num);
+        boolean seven = IsMultipleOfSeven(num);
+
+        if(three && seven)
+        {
+            System.out.println("fizzwoof");
+        }
+        else if(three && five)
+        {
+            System.out.println("fizzbuzz");
+        }
+        else if(five && seven)
+        {
+            System.out.println("buzzwoof");
+        }
+        else if(three)
+        {
+            System.out.println("fizz");
+        }
+        else if(five)
+        {
+            System.out.println("buzz");
+        }
+        else if(seven)
+        {
+            System.out.println("woof");
+        }
+        else
+        {
+            System.out.println(num);
         }
     }
-
 }
